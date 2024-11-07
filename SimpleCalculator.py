17ae9b5fd49532a6cb44b163390314953e02b47b
dheising <daltondheising@gmail.com>
2024-11-07 17:41:37 -0500

    Added Calculator Function

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -34,28 +34,70 @@ for widget in ['TButton', 'TEntry', 'TLabel', 'TCombobox']:
     style.configure(f'elder.{widget}', **base_style)
 
 
-#Close function
+#Close application function
 def Close():
     root.destroy()
 
-#Validate decimal value
-#def validate():
-    
+#Validate decimal values of both terms
+def validate():
+    if term_one.get().isdecimal() == True and term_two.get().isdecimal() == True:
+        calculate()
+    else:
+        error_val["text"] = "One of your terms has an invalid character or is empty!"
+
+
+#Calculates the end result
+def calculate():
+    error_val['text'] = ""
+    first_term = float(term_one.get())
+    second_term = float(term_two.get())
+    symbol = current_symbol.get()
+    match symbol:
+        case '+':
+            result = first_term + second_term
+        case '-':
+            result = first_term - second_term
+        case '*':
+            result = first_term * second_term
+        case '/':
+            if second_term == 0:
+                error_val["text"] = "Cannot Divide by 0!"
+            else:
+                result = first_term / second_term
+        case '%':
+            result = first_term % second_term
+    total_label['text'] = (f"The total is: {result}")
+    total_label.place(relx = 0.45, rely = 0.50, anchor= tk.CENTER)
+    pass
+
 
 #Shows welcome message at top of the screen
-calculator_title = ttk.Label(text="Welcome to the Simple Calculator!", font=('Times','25'))
+calculator_title = ttk.Label(text="Welcome to the Simple Calculator!",style= "elder.TLabel", font=('Times','25'))
 calculator_title.pack(side='top', anchor='center', pady=20)
 
-term_one = ttk.Entry(style="elder.TEntry")
+#Entry that is used as the first term for calculations
+term_one = ttk.Entry(style="elder.TEntry", foreground="black")
 term_one.place(relx=0.35, rely=0.25, relwidth=0.10, relheight=0.05)
 
+#Combobox that is used as the mathematical symbol for calculations
 current_symbol = tk.StringVar()
 math_symbol = ttk.Combobox(root, textvariable=current_symbol, state='readonly',style="elder.TCombobox")
 math_symbol['values'] = ('+', '-', '/', '*', '%')
 math_symbol.place(relx=0.48, rely=0.25, relwidth=0.04, relheight=0.05)
 
-term_two = ttk.Entry(style="elder.TEntry")
+#Entry that is used as the second term for calculations
+term_two = ttk.Entry(style="elder.TEntry", foreground= "black")
 term_two.place(relx=0.55, rely=0.25, relwidth=0.10, relheight=0.05)
+
+
+error_val = ttk.Label(style="elder.TLabel")
+error_val.place(anchor= tk.CENTER,relx = 0.5, rely=0.35)
+
+calculate_button = ttk.Button(root, text="Calculate", command=validate, style="elder.TButton")
+calculate_button.place(relx=0.4, rely=0.4, relwidth=0.2)
+
+total_label = ttk.Label(root, text="", style = "elder.TLabel")
+
 
 exit_button = ttk.Button(root, text="Exit Application", command=Close, style="elder.TButton")
 exit_button.place(relx=0.4, rely=0.85, relwidth=0.2)
